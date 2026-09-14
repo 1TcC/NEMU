ca9c0bf5d38c6d0b10d838620115943d131124e7
tracer-NEMU2020 <tjuics2020@163.com>
2026-09-14 15:06:59 +0000

    >  compile kernel 3025244371 l Linux 580403c53b65 6.8.0-138-generic #138-Ubuntu SMP PREEMPT_DYNAMIC Fri Jul 31 22:41:49 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  15:06:59 up  7:08,  0 users,  load average: 0.49, 0.29, 0.17 db5a3da62efbdc74d8e0c3f9485833098c38fc6c

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -30,35 +30,44 @@ uint32_t loader() {
 
 	elf = (void*)buf;
 
-	/* TODO: fix the magic number with the correct one */
-	const uint32_t elf_magic = 0xBadC0de;
-	uint32_t *p_magic = (void *)buf;
-	nemu_assert(*p_magic == elf_magic);
+const uint32_t elf_magic = 0x464c457f;
+uint32_t *p_magic = (void *)buf;
+nemu_assert(*p_magic == elf_magic);
 
-	/* Load each program segment */
-	panic("please implement me");
-	for(; true; ) {
-		/* Scan the program header table, load each segment into memory */
-		if(ph->p_type == PT_LOAD) {
+/* Load each program segment */
+int i;
 
-			/* TODO: read the content of the segment from the ELF file 
-			 * to the memory region [VirtAddr, VirtAddr + FileSiz)
-			 */
-			 
-			 
-			/* TODO: zero the memory region 
-			 * [VirtAddr + FileSiz, VirtAddr + MemSiz)
-			 */
+for(i = 0; i < elf->e_phnum; i ++) {
+	ph = (Elf32_Phdr *)(buf + elf->e_phoff +
+			i * elf->e_phentsize);
 
+	if(ph->p_type == PT_LOAD) {
+#ifdef HAS_DEVICE
+		ide_read((uint8_t *)ph->p_vaddr,
+				ELF_OFFSET_IN_DISK + ph->p_offset,
+				ph->p_filesz);
+#else
+		ramdisk_read((uint8_t *)ph->p_vaddr,
+				ELF_OFFSET_IN_DISK + ph->p_offset,
+				ph->p_filesz);
+#endif
+
+		nemu_assert(ph->p_filesz <= ph->p_memsz);
+
+		memset((void *)(ph->p_vaddr + ph->p_filesz),
+				0,
+				ph->p_memsz - ph->p_filesz);
 
 #ifdef IA32_PAGE
-			/* Record the program break for future use. */
-			extern uint32_t cur_brk, max_brk;
-			uint32_t new_brk = ph->p_vaddr + ph->p_memsz - 1;
-			if(cur_brk < new_brk) { max_brk = cur_brk = new_brk; }
-#endif
+		/* Record the program break for future use. */
+		extern uint32_t cur_brk, max_brk;
+		uint32_t new_brk = ph->p_vaddr + ph->p_memsz - 1;
+		if(cur_brk < new_brk) {
+			max_brk = cur_brk = new_brk;
 		}
+#endif
 	}
+}
 
 	volatile uint32_t entry = elf->e_entry;
 
