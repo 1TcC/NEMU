387535071f999cae2c8e9e647a7fec8fe0482c0c
tracer-NEMU2020 <tjuics2020@163.com>
2026-09-14 14:25:26 +0000

    >  compile NEMU 3025244371 l Linux 580403c53b65 6.8.0-138-generic #138-Ubuntu SMP PREEMPT_DYNAMIC Fri Jul 31 22:41:49 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  14:25:26 up  6:27,  0 users,  load average: 0.31, 0.19, 0.10 ab43253b71d2d3f5d5892f888089ab21fd08c4f6

--- nemu/src/cpu/exec/control/call.c
+++ nemu/src/cpu/exec/control/call.c
@@ -3,3 +3,17 @@
 #define DATA_BYTE 4
 #include "call-template.h"
 #undef DATA_BYTE
+
+make_helper(call_rm_l) {
+    int len = decode_rm_l(eip + 1) + 1;
+    uint32_t target = op_src->val;
+
+    cpu.esp -= 4;
+    swaddr_write(cpu.esp, 4, eip + len);
+
+    cpu.eip = target - len;
+
+    print_asm("call *%s", op_src->str);
+
+    return len;
+}
