f422d4cd009f95c3ea23c406a3a92f555437bc64
tracer-NEMU2020 <tjuics2020@163.com>
2026-09-14 09:05:31 +0000

    >  compile NEMU 3025244371 l Linux 580403c53b65 6.8.0-138-generic #138-Ubuntu SMP PREEMPT_DYNAMIC Fri Jul 31 22:41:49 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  09:05:31 up  1:07,  0 users,  load average: 0.08, 0.09, 0.08 f6c88f9167bf89c13b758b48f04ab05c4ff05b11

--- nemu/src/cpu/exec/control/jcc-template.h
+++ nemu/src/cpu/exec/control/jcc-template.h
@@ -3,15 +3,34 @@
 #define instr je
 
 static void do_execute() {
-    DATA_TYPE_S offset = (DATA_TYPE_S)op_src->val;
+	DATA_TYPE_S offset = (DATA_TYPE_S)op_src->val;
 
-    if(cpu.eflags.ZF) {
-        cpu.eip += offset;
-    }
+	if(cpu.eflags.ZF) {
+		cpu.eip += offset;
+	}
 
-    print_asm("je %x", cpu.eip + 1 + DATA_BYTE);
+	print_asm("je %x", cpu.eip + 1 + DATA_BYTE);
 }
 
 make_instr_helper(si)
+
+#undef instr
+
+
+#define instr jbe
+
+static void do_execute() {
+	DATA_TYPE_S offset = (DATA_TYPE_S)op_src->val;
+
+	if(cpu.eflags.CF || cpu.eflags.ZF) {
+		cpu.eip += offset;
+	}
+
+	print_asm("jbe %x", cpu.eip + 1 + DATA_BYTE);
+}
+
+make_instr_helper(si)
+
+#undef instr
 
 #include "cpu/exec/template-end.h"
