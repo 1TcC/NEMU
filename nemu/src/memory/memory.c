1801a4f7e6ef0f80ca1dbf550952437a71ccf680
tracer-NEMU2020 <tjuics2020@163.com>
2026-09-17 06:32:34 +0000

    >  compile NEMU 3025244371 l Linux 580403c53b65 6.8.0-138-generic #138-Ubuntu SMP PREEMPT_DYNAMIC Fri Jul 31 22:41:49 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  06:32:34 up  1:16,  0 users,  load average: 0.98, 1.15, 1.02 dc5b8a57b4ae76a225a70563d828c31371d0013b

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -5,70 +5,212 @@ uint32_t dram_read(hwaddr_t, size_t);
 void dram_write(hwaddr_t, size_t, uint32_t);
 
 #define CACHE_BLOCK_SIZE 64
-#define CACHE_SIZE       (64 * 1024)
-#define CACHE_WAY        8
-#define CACHE_SET_NUM    (CACHE_SIZE / CACHE_BLOCK_SIZE / CACHE_WAY)
+#define BLOCK_OFFSET_BITS 6
+
+/* L1: 64KB, 8-way */
+#define L1_CACHE_SIZE (64 * 1024)
+#define L1_WAY        8
+#define L1_SET_NUM    (L1_CACHE_SIZE / CACHE_BLOCK_SIZE / L1_WAY)
+#define L1_SET_BITS   7
+
+/* L2: 4MB, 16-way */
+#define L2_CACHE_SIZE (4 * 1024 * 1024)
+#define L2_WAY        16
+#define L2_SET_NUM    (L2_CACHE_SIZE / CACHE_BLOCK_SIZE / L2_WAY)
+#define L2_SET_BITS   12
+
 
 typedef struct {
 	bool valid;
 	uint32_t tag;
 	uint8_t data[CACHE_BLOCK_SIZE];
-} CacheLine;
-
-static CacheLine cache[CACHE_SET_NUM][CACHE_WAY];
+} L1CacheLine;
 
 
-/* Initialize L1 cache. */
-void init_cache() {
+typedef struct {
+	bool valid;
+	bool dirty;
+	uint32_t tag;
+	uint8_t data[CACHE_BLOCK_SIZE];
+} L2CacheLine;
+
+
+static L1CacheLine l1_cache[L1_SET_NUM][L1_WAY];
+static L2CacheLine l2_cache[L2_SET_NUM][L2_WAY];
+
+
+void init_cache(void) {
 	int i, j;
 
-	for(i = 0; i < CACHE_SET_NUM; i ++) {
-		for(j = 0; j < CACHE_WAY; j ++) {
-			cache[i][j].valid = false;
+	for(i = 0; i < L1_SET_NUM; i ++) {
+		for(j = 0; j < L1_WAY; j ++) {
+			l1_cache[i][j].valid = false;
 		}
 	}
 
+	for(i = 0; i < L2_SET_NUM; i ++) {
+		for(j = 0; j < L2_WAY; j ++) {
+			l2_cache[i][j].valid = false;
+			l2_cache[i][j].dirty = false;
+		}
+	}
 }
 
-static CacheLine *cache_fetch(hwaddr_t addr) {
-	uint32_t set = (addr >> 6) & 0x7f;
-	uint32_t tag = addr >> 13;
+
+
+static void l2_writeback(uint32_t set, L2CacheLine *line) {
+	int i;
+
+	if(!(line->valid && line->dirty)) {
+		return;
+	}
+
+	hwaddr_t block_addr =
+		(line->tag << (L2_SET_BITS + BLOCK_OFFSET_BITS))
+		| (set << BLOCK_OFFSET_BITS);
+
+	for(i = 0; i < CACHE_BLOCK_SIZE; i += 4) {
+		uint32_t data;
+
+		memcpy(&data, line->data + i, 4);
+		dram_write(block_addr + i, 4, data);
+	}
+
+	line->dirty = false;
+}
+
+
+
+static L2CacheLine *l2_fetch(hwaddr_t addr) {
+	uint32_t set =
+		(addr >> BLOCK_OFFSET_BITS) & (L2_SET_NUM - 1);
+
+	uint32_t tag =
+		addr >> (BLOCK_OFFSET_BITS + L2_SET_BITS);
 
 	int i;
 
-	for(i = 0; i < CACHE_WAY; i ++) {
-		if(cache[set][i].valid &&
-				cache[set][i].tag == tag) {
+	/* hit */
+	for(i = 0; i < L2_WAY; i ++) {
+		if(l2_cache[set][i].valid &&
+				l2_cache[set][i].tag == tag) {
 
-			return &cache[set][i];
+			return &l2_cache[set][i];
 		}
 	}
 
 	int victim = -1;
 
-	for(i = 0; i < CACHE_WAY; i ++) {
-		if(!cache[set][i].valid) {
+	for(i = 0; i < L2_WAY; i ++) {
+		if(!l2_cache[set][i].valid) {
 			victim = i;
 			break;
 		}
 	}
 
 	if(victim == -1) {
-		victim = rand() % CACHE_WAY;
+		victim = rand() % L2_WAY;
 	}
 
-	CacheLine *line = &cache[set][victim];
+	L2CacheLine *line = &l2_cache[set][victim];
+
+
+	l2_writeback(set, line);
 
 	hwaddr_t block_addr =
 		addr & ~(CACHE_BLOCK_SIZE - 1);
-	for(i = 0; i < CACHE_BLOCK_SIZE; i += 4) {
+
+
+		for(i = 0; i < CACHE_BLOCK_SIZE; i += 4) {
 		uint32_t data =
 			dram_read(block_addr + i, 4);
 
-		memcpy(line->data + i,
-				&data,
-				4);
+		memcpy(line->data + i, &data, 4);
 	}
+
+	line->tag = tag;
+	line->valid = true;
+	line->dirty = false;
+
+	return line;
+}
+
+
+
+static void l2_write(hwaddr_t addr,
+		size_t len, uint32_t data) {
+
+	size_t done = 0;
+
+	while(done < len) {
+		hwaddr_t cur_addr = addr + done;
+
+		uint32_t offset =
+			cur_addr & (CACHE_BLOCK_SIZE - 1);
+
+		size_t part_len =
+			CACHE_BLOCK_SIZE - offset;
+
+		if(part_len > len - done) {
+			part_len = len - done;
+		}
+
+
+		L2CacheLine *line =
+			l2_fetch(cur_addr);
+
+		memcpy(line->data + offset,
+				(uint8_t *)&data + done,
+				part_len);
+
+		line->dirty = true;
+
+		done += part_len;
+	}
+}
+
+
+
+
+static L1CacheLine *l1_fetch(hwaddr_t addr) {
+	uint32_t set =
+		(addr >> BLOCK_OFFSET_BITS) & (L1_SET_NUM - 1);
+
+	uint32_t tag =
+		addr >> (BLOCK_OFFSET_BITS + L1_SET_BITS);
+
+	int i;
+
+	for(i = 0; i < L1_WAY; i ++) {
+		if(l1_cache[set][i].valid &&
+				l1_cache[set][i].tag == tag) {
+
+			return &l1_cache[set][i];
+		}
+	}
+
+	int victim = -1;
+
+	for(i = 0; i < L1_WAY; i ++) {
+		if(!l1_cache[set][i].valid) {
+			victim = i;
+			break;
+		}
+	}
+
+	if(victim == -1) {
+		victim = rand() % L1_WAY;
+	}
+
+	L1CacheLine *line =
+		&l1_cache[set][victim];
+
+	L2CacheLine *l2_line =
+		l2_fetch(addr);
+
+	memcpy(line->data,
+			l2_line->data,
+			CACHE_BLOCK_SIZE);
 
 	line->tag = tag;
 	line->valid = true;
@@ -77,7 +219,7 @@ static CacheLine *cache_fetch(hwaddr_t addr) {
 }
 
 
-static uint32_t cache_read(hwaddr_t addr, size_t len) {
+static uint32_t l1_read(hwaddr_t addr, size_t len) {
 	uint32_t result = 0;
 	size_t done = 0;
 
@@ -94,8 +236,8 @@ static uint32_t cache_read(hwaddr_t addr, size_t len) {
 			part_len = len - done;
 		}
 
-		CacheLine *line =
-			cache_fetch(cur_addr);
+		L1CacheLine *line =
+			l1_fetch(cur_addr);
 
 		memcpy((uint8_t *)&result + done,
 				line->data + offset,
@@ -108,8 +250,7 @@ static uint32_t cache_read(hwaddr_t addr, size_t len) {
 }
 
 
-
-static void cache_write(hwaddr_t addr,
+static void l1_write(hwaddr_t addr,
 		size_t len, uint32_t data) {
 
 	size_t done = 0;
@@ -118,10 +259,12 @@ static void cache_write(hwaddr_t addr,
 		hwaddr_t cur_addr = addr + done;
 
 		uint32_t set =
-			(cur_addr >> 6) & 0x7f;
+			(cur_addr >> BLOCK_OFFSET_BITS)
+			& (L1_SET_NUM - 1);
 
 		uint32_t tag =
-			cur_addr >> 13;
+			cur_addr >>
+			(BLOCK_OFFSET_BITS + L1_SET_BITS);
 
 		uint32_t offset =
 			cur_addr & (CACHE_BLOCK_SIZE - 1);
@@ -135,27 +278,29 @@ static void cache_write(hwaddr_t addr,
 
 		int i;
 
-		for(i = 0; i < CACHE_WAY; i ++) {
-			if(cache[set][i].valid &&
-					cache[set][i].tag == tag) {
+		for(i = 0; i < L1_WAY; i ++) {
+			if(l1_cache[set][i].valid &&
+					l1_cache[set][i].tag == tag) {
 
-				memcpy(cache[set][i].data + offset,
-						(uint8_t *)&data + done,
-						part_len);
+				memcpy(
+					l1_cache[set][i].data + offset,
+					(uint8_t *)&data + done,
+					part_len);
 
 				break;
 			}
 		}
 
-
 		done += part_len;
 	}
-	dram_write(addr, len, data);
+
+	l2_write(addr, len, data);
 }
 
 
+
 uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
-	return cache_read(addr, len)
+	return l1_read(addr, len)
 		& (~0u >> ((4 - len) << 3));
 }
 
@@ -163,7 +308,7 @@ uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
 void hwaddr_write(hwaddr_t addr,
 		size_t len, uint32_t data) {
 
-	cache_write(addr, len, data);
+	l1_write(addr, len, data);
 }
 
 
